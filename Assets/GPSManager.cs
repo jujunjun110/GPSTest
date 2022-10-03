91bd69ea2bae62e6b65435209c91832f32028d33
Jun Ito <jujunjun110@gmail.com>
2022-10-03 13:24:42 +0900

    Add: initial commit

---
+++ Assets/GPSManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using TMPro;
+
+public class GPSManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timeStampText;
+    [SerializeField] private TextMeshProUGUI isEnabledText;
+    [SerializeField] private TextMeshProUGUI statusText;
+    [SerializeField] private TextMeshProUGUI latText;
+    [SerializeField] private TextMeshProUGUI lngText;
+
+    void Start()
+    {
+        InvokeRepeating(nameof(Do), 1f, 1f);
+        Input.location.Start();
+    }
+
+    void Do()
+    {
+        var dt = DateTime.Now;
+        var isEnabled = Input.location.isEnabledByUser;
+        var status = Input.location.status;
+
+        timeStampText.SetText(dt.ToString(CultureInfo.CurrentCulture));
+        isEnabledText.SetText($"IsEnabled: {isEnabled}");
+        statusText.SetText($"Status: {status}");
+
+        var latitude = Input.location.lastData.latitude;
+        var longitude = Input.location.lastData.longitude;
+        latText.SetText($"Latitude: {latitude}");
+        lngText.SetText($"Longitude: {longitude}");
+    }
+}
