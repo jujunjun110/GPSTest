4a02da49946e2f761a0523bfad2b64fc38cae96d
Jun Ito <jujunjun110@gmail.com>
2022-10-03 16:53:51 +0900

    Update: エラーメッセージ

--- Assets/GPSManager.cs
+++ Assets/GPSManager.cs
@@ -15,6 +15,7 @@ public class GPSManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI lngText;
     [SerializeField] private TextMeshProUGUI trueHeadingText;
     [SerializeField] private TextMeshProUGUI magneticHeadingText;
+    [SerializeField] private TextMeshProUGUI errorMessage;
 
     void Start()
     {
@@ -23,33 +24,41 @@ public class GPSManager : MonoBehaviour
 
     void UpdateDebugTexts()
     {
-        Input.location.Start();
-        Input.compass.enabled = true;
+        try
+        {
+            Input.location.Start();
+            Input.compass.enabled = true;
 
-        var dt = DateTime.Now;
-        var status = Input.location.status;
-        var isEnabled = Input.location.isEnabledByUser;
-        var isCompassEnabled = Input.compass.enabled;
+            var dt = DateTime.Now;
+            var status = Input.location.status;
+            var isEnabled = Input.location.isEnabledByUser;
+            var isCompassEnabled = Input.compass.enabled;
 
-        // var latitude = Input.location.lastData.latitude;
-        // var longitude = Input.location.lastData.longitude;
+            // var latitude = Input.location.lastData.latitude;
+            // var longitude = Input.location.lastData.longitude;
 
-        var lastData = Input.location.lastData;
-        System.Globalization.CultureInfo invariantCulture = System.Globalization.CultureInfo.InvariantCulture;
-        double latitude = double.Parse(lastData.latitude.ToString("R", invariantCulture), invariantCulture);
-        double longitude = double.Parse(lastData.longitude.ToString("R", invariantCulture), invariantCulture);
+            var lastData = Input.location.lastData;
+            System.Globalization.CultureInfo invariantCulture = System.Globalization.CultureInfo.InvariantCulture;
+            double latitude = double.Parse(lastData.latitude.ToString("R", invariantCulture), invariantCulture);
+            double longitude = double.Parse(lastData.longitude.ToString("R", invariantCulture), invariantCulture);
 
-        var trueHeading = Input.compass.trueHeading;
-        var magneticHeading = Input.compass.magneticHeading;
+            var trueHeading = Input.compass.trueHeading;
+            var magneticHeading = Input.compass.magneticHeading;
 
-        timeStampText.SetText(dt.ToString(CultureInfo.CurrentCulture));
-        isEnabledText.SetText($"IsEnabled: {isEnabled}");
-        isCompassEnabledText.SetText($"IsCompassEnabled: {isCompassEnabled}");
-        statusText.SetText($"Status: {status}");
+            timeStampText.SetText(dt.ToString(CultureInfo.CurrentCulture));
+            isEnabledText.SetText($"IsEnabled: {isEnabled}");
+            isCompassEnabledText.SetText($"IsCompassEnabled: {isCompassEnabled}");
+            statusText.SetText($"Status: {status}");
 
-        latText.SetText($"Latitude: {latitude}");
-        lngText.SetText($"Longitude: {longitude}");
-        trueHeadingText.SetText($"Heading: {trueHeading}");
-        magneticHeadingText.SetText($"Heading: {magneticHeading}");
+            latText.SetText($"Latitude: {latitude}");
+            lngText.SetText($"Longitude: {longitude}");
+            trueHeadingText.SetText($"Heading: {trueHeading}");
+            magneticHeadingText.SetText($"Heading: {magneticHeading}");
+            errorMessage.SetText($"Error: No Error");
+        }
+        catch (Exception e)
+        {
+            errorMessage.SetText($"Error: {e.Message}");
+        }
     }
 }
